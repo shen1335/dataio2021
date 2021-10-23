4df53bc3ab11e1acd661c8a6b76e20e69c0005de
ashenfood <70549343+ashenfood@users.noreply.github.com>
2021-10-23 11:23:48 -0400

    Added join code

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,4 +1,6 @@
 library(readr)
+library(tidyverse)
+library(dplyr)
 setwd("/Users/andrewshen/OSU Coding/DATA I:O/data-io-2021-velocity")
 dim_item_master <- read_csv("data/dim_item_master.csv")
 dim_dates <- read_csv("data/dim_dates.csv")
@@ -6,4 +8,7 @@ dim_items <- read_csv("data/dim_items.csv")
 fact_receipts <- read_csv("data/fact_receipts.csv")
 fact_invoice_items <- read_csv("data/fact_invoice_items.csv")
 
-joined_speedbump_df <- inner_join(filtered_speedbump, filtered_speedbump_subjects, by = c('Subject' = 'ParticipantID'))
+joined_items_data <- inner_join(dim_item_master, dim_items, by = c('mstr_item_id' = 'mstr_item_id'))
+
+joined_receipts_invoice <- inner_join(fact_receipts, fact_invoice_items, by = c('order_number' = 'order_number'))
+
