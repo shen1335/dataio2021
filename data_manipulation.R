df006b7925cfe5ce66c038884d08593cd2902e40
ashenfood <70549343+ashenfood@users.noreply.github.com>
2021-10-23 12:02:16 -0400

    Added cleaned item data

--- data_manipulation.R
+++ data_manipulation.R
@@ -9,8 +9,6 @@ fact_receipts <- read_csv("data/fact_receipts.csv")
 fact_invoice_items <- read_csv("data/fact_invoice_items.csv")
 
 joined_items_data <- inner_join(dim_item_master, dim_items, by = c('mstr_item_id' = 'mstr_item_id'))
-df<-select(joined_items_data, -c(group_food, dummy_is_fresh, ranking, packaging, match_status, material_donation_status, name.x, dummy_is_fresh, name_fa_31_cat))
-unique(df$name_category)
-view(fact_invoice_items)
-joined_receipts_invoice <- inner_join(fact_receipts, fact_invoice_items, by = c('order_number' = 'order_number'))
+cleaned_joined_items<-select(joined_items_data, -c(number.x, category_id, name_category, category_code, category_name, name.y, group_fresh, nickname, category_is_fresh, category_is_food, group_generic, commodity_id, fa_31_cat_id, name_combi, group_food, dummy_is_fresh, ranking, packaging, match_status, material_donation_status, name.x, dummy_is_fresh, name_fa_31_cat))
 
+write.csv(cleaned_joined_items,"joined_items.csv", row.names = FALSE)
